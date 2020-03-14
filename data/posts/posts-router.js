d696ddf61a62f7ebf15d9167302ce257d1f94c5c
alesslongaretti <alesslongaretti@gmail.com>
2020-03-13 23:03:07 -0400

    implemented POST that creates a post using the info sent inside the req body

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -5,7 +5,25 @@ const posts = require('../db.js');
 const router = express.Router();
 
 
-
+// POST
+router.post('/', (req, res) => {
+    const changes = req.body;
+ 
+     posts.insert(changes)
+         .then(post => {
+             if (!changes.title || !changes.contents) {
+                res.status(400).json({ success: false, message: 'Please provide title and contents for the post.'})
+                 } else {
+                    res.status(201).json({ success: true, post});
+             }
+         })
+         .catch(err => {
+             console.log(err);
+             res.status(5000).json({
+                 message: 'There was an error while saving the post to the database'
+             });
+         });
+ });
  
  // GET 
  
