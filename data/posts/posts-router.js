f5fe2d0796e5df77285754d1c0789c1ef08a5d41
alesslongaretti <alesslongaretti@gmail.com>
2020-03-13 22:55:15 -0400

    implemented GET

--- data/posts/posts-router.js
+++ data/posts/posts-router.js
@@ -7,5 +7,21 @@ const router = express.Router();
 
 
  
+ // GET 
+ 
+ router.get('/' , (req,res) => {
+     console.log(req.query);
+     posts.find(req.query)
+         .then(post => {
+             res.status(201).json(post);
+         })
+         .catch(err => {
+             console.log(err)
+             res.status(500).json({ 
+                 message: 'The posts information could not be retrieved.'
+             });
+         });
+ });
+ 
  
  module.exports = router;
